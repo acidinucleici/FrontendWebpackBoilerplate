d9a22b1752fcd8bf8ab4bd30f4dab7507874d45b
acidinucleici <acidinucleici@gmail.com>
2018-05-08 17:39:31 +0200

    Aggiunto autoprefix

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     plugins: [
         require('precss'), 
-        require('autoprefixer'), 
+        require('autoprefixer')({
+	        browsers: ['last 2 versions']
+        }),
         require('postcss-font-magician')({
             hosted: ['./src/fonts']
         })
